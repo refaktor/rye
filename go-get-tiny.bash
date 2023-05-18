e83137ea5641ff9c1bae6cf35dcf14c691e6d6a2
Refaktor <janko.itm+refaktor@gmail.com>
2023-05-18 12:15:32 +0200

    Update go-get-tiny.bash

--- go-get-tiny.bash
+++ go-get-tiny.bash
@@ -3,3 +3,5 @@ go get github.com/refaktor/liner@v1.2.6   # library for REPL
 go get golang.org/x/net/html       # for html parsin - will probably remove for b_tiny
 go get github.com/pkg/profile      # for runtime profiling - will probably remove for b_tiny
 go get github.com/pkg/term         # 
+go get github.com/drewlanenga/govector # for vector datatype
+go get github.com/jinzhu/copier
