77673f3f722cc66200ccdd5c49923ef5d5914538
Refaktor <janko.itm+refaktor@gmail.com>
2023-05-18 11:40:50 +0200

    Update go-get-tiny.bash

--- go-get-tiny.bash
+++ go-get-tiny.bash
@@ -1,5 +1,5 @@
 go get github.com/refaktor/go-peg  # PEG parser (rye loader)
-go get github.com/refaktor/liner   # library for REPL
+go get github.com/refaktor/liner@v1.2.6   # library for REPL
 go get golang.org/x/net/html       # for html parsin - will probably remove for b_tiny
 go get github.com/pkg/profile      # for runtime profiling - will probably remove for b_tiny
 go get github.com/pkg/term         # 
