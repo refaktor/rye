0b8be17616f7d5170ba071b6dcd459b0c6265036
Pratik Mota <pratik@pratikmota.com>
2023-11-29 15:20:18 +0530

    error handling in raylib file

--- evaldo/builtins_raylib.go
+++ evaldo/builtins_raylib.go
@@ -1,3 +1,4 @@
+//go:build b_raylib
 // +build b_raylib
 
 package evaldo
@@ -5,14 +6,15 @@ package evaldo
 import (
 	"rye/env"
 
-	"github.com/gen2brain/raylib-go/raylib"
+	rl "github.com/gen2brain/raylib-go/raylib"
 )
 
 var Builtins_raylib = map[string]*env.Builtin{
 
 	"raylib-init": {
 		Argsn: 3,
-		Fn: func(env1 *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
+		Doc:   "TODODOC",
+		Fn: func(ps *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
 			switch width := arg0.(type) {
 			case env.Integer:
 				switch height := arg1.(type) {
@@ -20,33 +22,35 @@ var Builtins_raylib = map[string]*env.Builtin{
 					switch title := arg2.(type) {
 					case env.String:
 						rl.InitWindow(int32(width.Value), int32(height.Value), title.Value)
-						return *env.NewNative(env1.Idx, 1, "raylib")
+						return *env.NewNative(ps.Idx, 1, "raylib")
 					default:
-						return env.NewError("arg 1 should be Integer")
+						return MakeArgError(ps, 3, []env.Type{env.StringType}, "raylib-init")
 					}
 				default:
-					return env.NewError("arg 2 should be Integer")
+					return MakeArgError(ps, 2, []env.Type{env.IntegerType}, "raylib-init")
 				}
 			default:
-				return env.NewError("arg 3 should be String")
+				return MakeArgError(ps, 1, []env.Type{env.IntegerType}, "raylib-init")
 			}
 		},
 	},
 	"raylib-set-target-fps": {
 		Argsn: 1,
-		Fn: func(env1 *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
+		Doc:   "TODODOC",
+		Fn: func(ps *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
 			switch fps := arg0.(type) {
 			case env.Integer:
 				rl.SetTargetFPS(int32(fps.Value))
 				return arg0
 			default:
-				return env.NewError("arg 1 should be Integer")
+				return MakeArgError(ps, 1, []env.Type{env.IntegerType}, "raylib-set-target-fps")
 			}
 		},
 	},
 	"raylib-window-should-close": {
 		Argsn: 0,
-		Fn: func(env1 *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
+		Doc:   "TODODOC",
+		Fn: func(ps *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
 			if rl.WindowShouldClose() {
 				return env.Integer{1}
 			} else {
@@ -56,6 +60,7 @@ var Builtins_raylib = map[string]*env.Builtin{
 	},
 	"raylib-main-loop": {
 		Argsn: 1,
+		Doc:   "TODODOC",
 		Fn: func(ps *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
 			switch bloc := arg0.(type) {
 			case env.Block:
@@ -68,35 +73,39 @@ var Builtins_raylib = map[string]*env.Builtin{
 				ps.Ser = ser
 				return ps.Res
 			default:
-				return env.NewError("arg 1 should be Block of code")
+				return MakeArgError(ps, 1, []env.Type{env.BlockType}, "raylib-main-loop")
 			}
 		},
 	},
 
 	"raylib-begin-drawing": {
 		Argsn: 0,
-		Fn: func(env1 *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
+		Doc:   "TODODOC",
+		Fn: func(ps *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
 			rl.BeginDrawing()
 			return arg0
 		},
 	},
 	"raylib-end-drawing": {
 		Argsn: 0,
-		Fn: func(env1 *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
+		Doc:   "TODODOC",
+		Fn: func(ps *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
 			rl.EndDrawing()
 			return arg0
 		},
 	},
 	"raylib-close-window": {
 		Argsn: 0,
-		Fn: func(env1 *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
+		Doc:   "TODODOC",
+		Fn: func(ps *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
 			rl.CloseWindow()
 			return arg0
 		},
 	},
 	"raylib-draw-circle": {
 		Argsn: 4,
-		Fn: func(env1 *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
+		Doc:   "TODODOC",
+		Fn: func(ps *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
 			switch cx := arg0.(type) {
 			case env.Integer:
 				switch cy := arg1.(type) {
@@ -109,43 +118,46 @@ var Builtins_raylib = map[string]*env.Builtin{
 							rl.DrawCircle(int32(cx.Value), int32(cy.Value), float32(r.Value), col.Value.(rl.Color))
 							return arg0
 						default:
-							return env.NewError("arg 4 should be Native (Color)")
+							return MakeArgError(ps, 4, []env.Type{env.NativeType}, "raylib-draw-circle")
 						}
 					default:
-						return env.NewError("arg 3 should be Integer")
+						return MakeArgError(ps, 3, []env.Type{env.IntegerType}, "raylib-draw-circle")
 					}
 				default:
-					return env.NewError("arg 2 should be Integer")
+					return MakeArgError(ps, 2, []env.Type{env.IntegerType}, "raylib-draw-circle")
 				}
 			default:
-				return env.NewError("arg 1 should be Integer")
+				return MakeArgError(ps, 1, []env.Type{env.IntegerType}, "raylib-draw-circle")
 			}
 		},
 	},
 
 	"raylib-clear-background": {
 		Argsn: 1,
-		Fn: func(env1 *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
+		Doc:   "TODODOC",
+		Fn: func(ps *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
 			switch col := arg0.(type) {
 			case env.Native:
 				rl.ClearBackground(col.Value.(rl.Color))
 				return arg0
 			default:
-				return env.NewError("arg 2 should be Native (color)")
+				return MakeArgError(ps, 1, []env.Type{env.NativeType}, "raylib-clear-background")
 			}
 		},
 	},
 
 	"raylib-gold": {
 		Argsn: 0,
-		Fn: func(env1 *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
-			return *env.NewNative(env1.Idx, rl.Gold, "rl-color")
+		Doc:   "TODODOC",
+		Fn: func(ps *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
+			return *env.NewNative(ps.Idx, rl.Gold, "rl-color")
 		},
 	},
 	"raylib-black": {
 		Argsn: 0,
-		Fn: func(env1 *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
-			return *env.NewNative(env1.Idx, rl.Black, "rl-color")
+		Doc:   "TODODOC",
+		Fn: func(ps *env.ProgramState, arg0 env.Object, arg1 env.Object, arg2 env.Object, arg3 env.Object, arg4 env.Object) env.Object {
+			return *env.NewNative(ps.Idx, rl.Black, "rl-color")
 		},
 	},
 }
