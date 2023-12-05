f2c588b6b932e502271d819aaab9e44f058f3b50
refaktor <janko.itm+refaktor@gmail.com>
2023-12-05 13:08:16 +0100

    added go-get-more for typical build with more deps

---
+++ go-get-more.bash
@@ -0,0 +1,8 @@
+go get github.com/go-telegram-bot-api/telegram-bot-api
+go get github.com/gobwas/ws
+go get github.com/gobwas/ws/wsutil
+go get github.com/gorilla/sessions
+go get github.com/mattn/go-sqlite3
+go get github.com/mhale/smtpd
+go get github.com/thomasberger/parsemail
+go get go.mongodb.org/mongo-driver/bson
