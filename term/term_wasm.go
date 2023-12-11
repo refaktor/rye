a1f5778c8b6faad1a71418d69e5a7adb9dcab7f9
Štefan Baebler <stefan.baebler@gmail.com>
2023-12-11 20:02:41 +0100

    Enable wasm build in CI, dummy term package non-implementation
    The dummy term package is needed in "display" builtin.

---
+++ term/term_wasm.go
@@ -0,0 +1,33 @@
+//go:build wasm
+// +build wasm
+
+package term
+
+import (
+	"rye/env"
+)
+
+// DisplayBlock is dummy non-implementation for wasm for display builtin
+func DisplayBlock(bloc env.Block, idx *env.Idxs) (env.Object, bool) {
+	panic("unimplemented")
+}
+
+// DisplayDict is dummy non-implementation for wasm for display builtin
+func DisplayDict(bloc env.Dict, idx *env.Idxs) (env.Object, bool) {
+	panic("unimplemented")
+}
+
+// DisplaySpreadsheetRow is dummy non-implementation for wasm for display builtin
+func DisplaySpreadsheetRow(bloc env.SpreadsheetRow, idx *env.Idxs) (env.Object, bool) {
+	panic("unimplemented")
+}
+
+// DisplayTable is dummy non-implementation for wasm for display builtin
+func DisplayTable(bloc env.Spreadsheet, idx *env.Idxs) (env.Object, bool) {
+	panic("unimplemented")
+}
+
+// SaveCurPos is dummy non-implementation for wasm for display builtin
+func SaveCurPos() {
+	panic("unimplemented")
+}
