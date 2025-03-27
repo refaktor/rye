d9046947957479f161ab66a6d9d2078b2db527c4
refaktor <janko.itm+refaktor@gmail.com>
2025-03-27 19:37:15 +0100

    separate term GetChar* functions for windows

--- term/term.go
+++ term/term.go
@@ -11,8 +11,6 @@ import (
 
 	"github.com/refaktor/rye/env"
 	"github.com/refaktor/rye/util"
-
-	"github.com/pkg/term"
 )
 
 func DisplayBlock(bloc env.Block, idx *env.Idxs) (env.Object, bool) {
@@ -879,92 +877,6 @@ func CurLeft(n int) {
 	fmt.Printf("\x1b[%dD", n)
 }
 
-// From https://github.com/paulrademacher/climenu/blob/master/getchar.go
-
-func GetChar() (ascii int, keyCode int, err error) {
-	t, _ := term.Open("/dev/tty")
-	if err = term.RawMode(t); err != nil {
-		return
-	}
-	bytes := make([]byte, 3)
-
-	var numRead int
-	numRead, err = t.Read(bytes)
-	if err != nil {
-		return
-	}
-	if numRead == 3 && bytes[0] == 27 && bytes[1] == 91 {
-		// Three-character control sequence, beginning with "ESC-[".
-
-		// Since there are no ASCII codes for arrow keys, we use
-		// Javascript key codes.
-		if bytes[2] == 65 {
-			// Up
-			keyCode = 38
-		} else if bytes[2] == 66 {
-			// Down
-			keyCode = 40
-		} else if bytes[2] == 67 {
-			// Right
-			keyCode = 39
-		} else if bytes[2] == 68 {
-			// Left
-			keyCode = 37
-		}
-	} else if numRead == 1 {
-		ascii = int(bytes[0])
-	}
-	// else {
-	// Two characters read??
-	// }
-	if err = t.Restore(); err != nil {
-		return
-	}
-	t.Close()
-	return
-}
-
-func GetChar2() (letter string, ascii int, keyCode int, err error) {
-	t, _ := term.Open("/dev/tty")
-	if err = term.RawMode(t); err != nil {
-		return
-	}
-	bytes := make([]byte, 3)
-
-	var numRead int
-	numRead, err = t.Read(bytes)
-	if err != nil {
-		return
-	}
-	if numRead == 3 && bytes[0] == 27 && bytes[1] == 91 {
-		// Three-character control sequence, beginning with "ESC-[".
-
-		// Since there are no ASCII codes for arrow keys, we use
-		// Javascript key codes.
-		if bytes[2] == 65 {
-			// Up
-			keyCode = 38
-		} else if bytes[2] == 66 {
-			// Down
-			keyCode = 40
-		} else if bytes[2] == 67 {
-			// Right
-			keyCode = 39
-		} else if bytes[2] == 68 {
-			// Left
-			keyCode = 37
-		}
-	} else if numRead == 1 {
-		ascii = int(bytes[0])
-		letter = string(bytes[0])
-	} else if numRead == 2 {
-		letter = string(bytes[0:2])
-	} else if numRead == 3 {
-		letter = string(bytes)
-	}
-	if err = t.Restore(); err != nil {
-		return
-	}
-	t.Close()
-	return
-}
+// GetChar and GetChar2 functions are implemented in platform-specific files:
+// - term_unix.go for Unix/Linux systems
+// - term_windows.go for Windows systems
