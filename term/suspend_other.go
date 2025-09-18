9507f5653385d699eec9281e68b0b250f2612863
refaktor <janko.itm+refaktor@gmail.com>
2025-09-18 09:25:25 +0200

    fixing issue in windows compilation

--- term/suspend_other.go
+++ term/suspend_other.go
@@ -4,6 +4,16 @@ package term
 
 import "fmt"
 
+// Global variable to hold the terminal restoration function (no-op on non-Unix systems)
+var terminalRestoreFunc func() error
+
+// SetTerminalRestoreFunc allows external packages to register a function
+// that will be called to restore terminal state after suspension (no-op on non-Unix systems)
+func SetTerminalRestoreFunc(restoreFunc func() error) {
+	// No-op on non-Unix systems since they don't support Unix-style process suspension
+	terminalRestoreFunc = restoreFunc
+}
+
 // SuspendProcess implements fallback process suspension for non-Unix systems
 func SuspendProcess() error {
 	fmt.Println("[ Process suspended with Ctrl+Z - Use 'fg' to resume ]")
