2ac1ae4f0965011e169ee98c53722660fccf84ed
refaktor <janko.itm+refaktor@gmail.com>
2025-11-25 12:58:51 +0100

    redoing all examples

--- examples/adventofcode/2023/1/sample.txt
+++
@@ -1,4 +0,0 @@
-1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet
--- examples/examples-wip/example_ws_fail.r
+++
@@ -1,4 +0,0 @@
-new-server ":8080"
- |handle "/" fn { w r } { write w "Hello from Rye function!" }  
- |handle-ws "/ping" fn { s ctx } { print read s ctx print "before fail" failure "failure in handler" |print print "after fail" }   
- |serve
